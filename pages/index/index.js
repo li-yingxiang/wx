607ee3aa1400c3bc0addc2e4a34ea3b5da9545fe
li-yingxiang <501613345@qq.com>
2021-09-17 07:51:34 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,53 @@
 // index.js
 // 获取应用实例
+import {
+  request
+} from "../../request/index.js";
 const app = getApp()
-
 Page({
-  data:{
-    swiperList:[]
+  data: {
+    // 轮播
+    swiperList: [],
+    // 导航
+    cateList:[],
+    floorList:[]
   },
-  onLoad(){
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result)=>{
-          console.log(result.data.message)
-        this.setData({
-            swiperList:result.data.message
-        }) 
-      },
-    });
+  onLoad() {
+    // 获取轮播图
+    this.getSwiperList()
+    // 获取轮播
+    this.getCateList()
+    this.getFloorList()
   },
-  onShow(){
+  onShow() {
     console.log(this.data.swiperList)
+  },
+  getSwiperList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+    }).then((result) => {
+      this.setData({
+        swiperList: result.data.message
+      })
+    })
+  },
+  getCateList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+    }).then((result) => {
+      this.setData({
+        cateList: result.data.message
+      })
+    })
+  },
+  getFloorList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+    }).then((result) => {
+      console.log(result)
+      this.setData({
+        floorList: result.data.message
+      })
+    })
   }
 })
