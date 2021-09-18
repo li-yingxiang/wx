d2ddc5bbac5d2ee8b48aba34bf4eadd614a5b1d4
li-yingxiang <501613345@qq.com>
2021-09-18 08:39:52 +0800

    商品分类

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,23 +1,44 @@
 // pages/category/index.js
+import {
+    request
+} from "../../request/index.js";
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        // 左侧菜单数据
+        leftMenuList: [],
+        // 右侧菜单数据
+        rightContent: []
     },
-
+    Cates: [],
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.getCass()
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
+    getCass() {
+        request({
+            url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+        }).then((res) => {
+            console.log(res.data.mess)
+            this.Cates = res.data.message;
+            console.log(this.Cates)
+            let leftMenuList = this.Cates.map(v=>v.cat_name)
+            let rightContent = this.Cates[0].children
+            this.setData({
+                leftMenuList,
+                rightContent
+            })
+        })
+    },
     onReady: function () {
 
     },
